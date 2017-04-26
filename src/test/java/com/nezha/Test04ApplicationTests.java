c6a44816e62fe974a2e59065f0bd2a88522f31ef
nezha <1061243497@qq.com>
2017-04-27 00:07:28 +0800

    Initial commit

---
+++ src/test/java/com/nezha/Test04ApplicationTests.java
@@ -0,0 +1,27 @@
+package com.nezha;
+
+import com.nezha.domain.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test04ApplicationTests {
+
+	@Autowired
+	StringRedisTemplate stringRedisTemplate;
+
+	@Test
+	public void test() throws Exception {
+		// 保存字符串
+		stringRedisTemplate.opsForValue().set("aaa", "111");
+		Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+	}
+
+}
